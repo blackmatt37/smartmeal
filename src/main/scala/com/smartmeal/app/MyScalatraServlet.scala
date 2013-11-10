f3413b9aa60b8bb39425a871c0e3f27ce653f820
Matthew Kramer <matthewkram@gmail.com>
2013-11-09 22:24:37 -0500

    added simple lp problem

--- src/main/scala/com/smartmeal/app/MyScalatraServlet.scala
+++ src/main/scala/com/smartmeal/app/MyScalatraServlet.scala
@@ -8,8 +8,30 @@ import oscar.algebra._
 
 class MyScalatraServlet extends SmartmealStack {
 
+object MyLPProblem {
+   def main(args: Array[String]) {
+      val lp = LPSolver() 
+      val x0 = LPVar(lp,"x0",0,40) // can take value in continuous interval [0,40]
+      val x1 = LPVar(lp,"x1",0, 1000) 
+      val x2 = LPVar(lp,"x2",0 ,17) 
+      val x3 = LPVar(lp,"x3",2,3)  
+     
+      lp.maximize(x0+2*x1+3*x2+x3) subjectTo {
+    lp.add(-1*x0 + x1 + x2 + 10*x3 <= 20)
+    lp.add(x0 - 3.0*x1 + x2 <= 30)
+    lp.add(x1 - 3.5*x3 == 0 )
+      }
+      // println("objective"+lp.getObjectiveValue()) 
+      // println(x1.getValue())
+   }
+}
+
+
+
+
   get("/") {
     contentType = "text/html"
+    MyLPProblem.main(Array())
     jade("home.jade")
   }
   get("/home") {
