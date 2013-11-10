0c1f8bf528da17a67e6992a5e5d646529d712e32
Matthew Kramer <matthewkram@gmail.com>
2013-11-10 04:17:54 -0500

    DIE ALL DB

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,29 +1,10 @@
-import com.mchange.v2.c3p0.ComboPooledDataSource
 import org.slf4j.LoggerFactory
-import scala.slick.session.Database
 import com.smartmeal.app._
 import org.scalatra._
 import javax.servlet.ServletContext
 
-
-
 class ScalatraBootstrap extends LifeCycle {
-	val logger = LoggerFactory.getLogger(getClass)
-
-  	val cpds = new ComboPooledDataSource
-  	logger.info("Created c3p0 connection pool")
   	override def init(context: ServletContext) {
-  		val db = Database.forDataSource(cpds)
     	context.mount(new MyScalatraServlet, "/*")
   	}
-
-  	private def closeDbConnection() {
-    	logger.info("Closing c3po connection pool")
-    	cpds.close
-  	}
-
-  	override def destroy(context: ServletContext) {
-    	super.destroy(context)
-    	closeDbConnection
-    }
 }
--- src/main/scala/com/smartmeal/app/MyScalatraServlet.scala
+++ src/main/scala/com/smartmeal/app/MyScalatraServlet.scala
@@ -29,15 +29,62 @@ object MyLPProblem {
    }
 }
 
+object calc {
+  def protein(weight: Int, activity: Int): Double = {
+    if(activity == 0) weight*(2.0/5) else {
+      if(activity == 1) weight*(1.0/2) else {
+        if(activity == 2) weight*(3.0/5) else weight*(7.0/10)
+      }
+    }
+  }
+
+  def calories(age: Int, gender: String, activity: Int, height: Int, weight: Int): Double = {
+    if(gender equals "male") {
+      val prelim: Double = 66.47 + 6.25*weight + 12.7*height - 6.76*age
+      if(activity==0) prelim*1.3 else {if(activity==1) prelim*1.6 else {if(activity==2) prelim*1.8 else prelim*2.2}}
+    }
+    else {
+      val prelim: Double = 65.51 + 4.386*weight + 4.6736*height - 4.68*age
+      if(activity==0) prelim*1.3 else {if(activity==1) prelim*1.5 else {if(activity==2) prelim*1.7 else prelim*1.9}}
+    }
+  }
+
+  def inches(feet: Int, inch: Int): Int = {
+    feet*12 + inch
+  }
+
+  def calcium(age: Int, gender: String): Int = {
+     0
+  }
+
+  def fat(age: Int, gender: String, activity: Int, height: Int, weight: Int): Double = {
+    calories(age, gender, activity, height, weight)/36.0
+  }
+
+  def carbs(age: Int, gender: String, activity: Int, height: Int, weight: Int): Double = {
+    calories(age, gender, activity, height, weight)*0.15378
+  }
+
+  def sugar(gender: String): Int = {
+    if(gender equals "male") 30 else 20
+  }
+
+}
+
 
 
   get("/") {
     contentType = "text/html"
     jade("home.jade")
   }
+  get("/db") {
+    val db : SQLiteConnection = new SQLiteConnection(new File("/tmp/database"));
+    db.open(true)
+    db.exec("CREATE TABLE Test (test varchar(255));")
+
+  }
   get("/home") {
       contentType = "text/html"
-      val db : SQLiteConnection = new SQLiteConnection(new File("/tmp/database"));
       jade("home.jade")
   }
   post("/home") {
